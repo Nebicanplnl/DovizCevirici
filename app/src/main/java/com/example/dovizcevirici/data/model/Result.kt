d1e94daab91021d6773f7366723ace8d13ff7764
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-23 17:33:09 +0300

    project

---
+++ app/src/main/java/com/example/dovizcevirici/data/model/Result.kt
@@ -0,0 +1,10 @@
+package com.example.dovizcevirici.data.model
+
+import com.example.dovizcevirici.domain.model.Gold
+
+data class Result(
+    val buy: String,
+    val name: String,
+    val sell: String
+)
+
