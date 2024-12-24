d934f3c8472bc621124460d57b3168a246c9fc2f
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-24 23:19:14 +0300

    GoldState update

--- app/src/main/java/com/example/dovizcevirici/presentation/ui/gold/GoldState.kt
+++ app/src/main/java/com/example/dovizcevirici/presentation/ui/gold/GoldState.kt
@@ -1,6 +1,5 @@
 package com.example.dovizcevirici.presentation.ui.gold
 
 import com.example.dovizcevirici.data.model.GoldDto
-import com.example.dovizcevirici.domain.model.Gold
 
 data class GoldState(val isLoading: Boolean = false, val gold: GoldDto = GoldDto(), val error: String = "")
