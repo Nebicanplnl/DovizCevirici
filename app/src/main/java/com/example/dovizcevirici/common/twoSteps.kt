b0ebda7ce9b5c362c86d7e544fcd87c9a6f8a7a5
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-27 18:05:41 +0300

    .

--- app/src/main/java/com/example/dovizcevirici/common/twoSteps.kt
+++ app/src/main/java/com/example/dovizcevirici/common/twoSteps.kt
@@ -2,7 +2,7 @@ package com.example.dovizcevirici.common
 
 fun String.formatAsTwoDecimal(): String {
     return try {
-        String.format("%.3f", this.toDouble()) // String -> Double -> 2 Ondalık Basamak
+        String.format("%.3f", this.toDouble()) // String -> Double -> 3 Ondalık Basamak
     } catch (e: Exception) {
         "N/A" // Eğer çevrilemezse varsayılan değer
     }
