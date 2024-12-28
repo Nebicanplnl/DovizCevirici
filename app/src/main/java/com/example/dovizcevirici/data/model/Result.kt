bef42299940cb2c948a49714b7085bebc55e0a89
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-29 02:37:14 +0300

    Result Updated

--- app/src/main/java/com/example/dovizcevirici/data/model/Result.kt
+++ app/src/main/java/com/example/dovizcevirici/data/model/Result.kt
@@ -6,6 +6,7 @@ import com.google.gson.annotations.SerializedName
 data class Result(
    @SerializedName("buying") val buy: String,
    @SerializedName("name")val name: String,
-   @SerializedName("selling") val sell: String
+   @SerializedName("selling") val sell: String,
+   @SerializedName("code") val code: String? = null
 )
 
