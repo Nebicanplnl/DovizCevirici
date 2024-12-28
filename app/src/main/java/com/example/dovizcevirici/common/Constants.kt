5d4252babef372ff4ed9c70319a4c52bd8382d3e
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-29 02:31:52 +0300

    Code refactor

--- app/src/main/java/com/example/dovizcevirici/common/Constants.kt
+++ app/src/main/java/com/example/dovizcevirici/common/Constants.kt
@@ -5,6 +5,8 @@ object Constants {
     const val API_KEY = "apikey 6qDy2cXLIHDDrPHG79RnOl:2aLFCo56Ega9Jvb3BbF94n"
 
     const val BASE_URL = "https://api.collectapi.com/"
+    //https://api.collectapi.com/economy/exchange?base=USD&to=EUR&int=100
+    //https://api.collectapi.com/economy/exchange?int=10&to=EUR&base=USD
 
     const val CONTENT_TYPE = "application/json"
 
