a2811e60e79ffb8ea0c72c3a7d7c48920177bfa2
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-28 18:09:30 +0300

    CurrencyState refactor

--- app/src/main/java/com/example/dovizcevirici/presentation/ui/currency/CurrencyState.kt
+++ app/src/main/java/com/example/dovizcevirici/presentation/ui/currency/CurrencyState.kt
@@ -2,4 +2,8 @@ package com.example.dovizcevirici.presentation.ui.currency
 
 import com.example.dovizcevirici.data.model.GoldDto
 
-data class CurrencyState(val isLoading: Boolean = false, val gold: GoldDto = GoldDto(), val error: String = "")
+data class CurrencyState(
+    val isLoading: Boolean = false,
+    val gold: GoldDto = GoldDto(),
+    val error: String = ""
+)
