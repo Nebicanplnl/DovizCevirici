cb6bf1bbdfba3f13a4749c223ae987d4f0fb5da4
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-29 02:32:24 +0300

    Converter Updated

--- app/src/main/java/com/example/dovizcevirici/data/model/ConverterDto.kt
+++ app/src/main/java/com/example/dovizcevirici/data/model/ConverterDto.kt
@@ -1,6 +1,6 @@
 package com.example.dovizcevirici.data.model
 
 data class ConverterDto(
-    val result: List<ResultX> = emptyList(),
+    val result: ResultX,
     val success: Boolean
 )
--- app/src/main/java/com/example/dovizcevirici/presentation/ui/converter/ConverterFragment.kt
+++ app/src/main/java/com/example/dovizcevirici/presentation/ui/converter/ConverterFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.dovizcevirici.R
-import com.example.dovizcevirici.common.viewBinding
+import com.example.dovizcevirici.common.ext.viewBinding
 import com.example.dovizcevirici.databinding.FragmentConverterBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -37,7 +37,7 @@ class ConverterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getCurrency()
+        viewModel.getConverter()
 
         launchRepeatWithViewLifecycle {
             viewModel.uiState.collect {
@@ -55,12 +55,37 @@ class ConverterFragment : Fragment() {
             }
         }
 
+        binding.editTextFrom.addTextChangedListener { it ->
+            binding.textTo.text = it.toString()
+        }
+
         binding.spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 // Seçilen öğe ile ilgili işlemler burada yapılabilir
                 val selectedItem = parent.getItemAtPosition(position).toString()
+
                 // Örneğin, seçilen öğeyi loglayabiliriz
                 Log.d("Spinner", "Seçilen öğe: $selectedItem")
+
+                viewModel.base = selectedItem
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Hiçbir şey seçilmediğinde yapılacak işlemler
+                Log.d("Spinner", "Hiçbir öğe seçilmedi")
+            }
+        }
+
+        binding.spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                // Seçilen öğe ile ilgili işlemler burada yapılabilir
+                val selectedItem = parent.getItemAtPosition(position).toString()
+
+                // Örneğin, seçilen öğeyi loglayabiliriz
+                Log.d("Spinner", "Seçilen öğe: $selectedItem")
+
+                viewModel.to = selectedItem
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
@@ -74,17 +99,30 @@ class ConverterFragment : Fragment() {
             binding.textTo.text = ""
         }
 
+        // Çevir butonuna tıklanırsa EditText'teki veriyi TextView'e yazdır
+        binding.converterButton.setOnClickListener {
+            val amount = binding.editTextFrom.text.toString()
+            if (amount.isNotEmpty()) {
+                binding.textTo.text = "$amount"
+            } else {
+                binding.textTo.text = "Lütfen bir miktar girin"
+            }
+            viewModel.getExchange()
+
+        }
+
     }
 
-    inline fun Fragment.launchRepeatWithViewLifecycle(
-        minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-        crossinline block: suspend CoroutineScope.() -> Unit
-    ) {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
-                block()
+        inline fun Fragment.launchRepeatWithViewLifecycle(
+            minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+            crossinline block: suspend CoroutineScope.() -> Unit
+        ) {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
+                    block()
+                }
             }
         }
-    }
 
-}
+
+    }
