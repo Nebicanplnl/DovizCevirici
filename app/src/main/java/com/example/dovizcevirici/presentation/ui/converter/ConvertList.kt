ffbce5da5c4a97c0acff9e943d742ba4e591c099
Nebican Planalı <nebicanplanali@gmail.com>
2024-12-30 04:50:34 +0300

    ConverterList added

---
+++ app/src/main/java/com/example/dovizcevirici/presentation/ui/converter/ConvertList.kt
@@ -0,0 +1,42 @@
+package com.example.dovizcevirici.presentation.ui.converter
+
+object ConvertList {
+
+    val exchangeRates = mapOf(
+        "USD" to 35.2617,   // 1 USD = 35.2617 TRY
+        "EUR" to 36.7627,   // 1 EUR = 36.7627 TRY
+        "GBP" to 44.3902,   // 1 GBP = 44.3902 TRY
+        "JPY" to 0.18,      // 1 JPY = 0.18 TRY
+        "CHF" to 39.1059,   // 1 CHF = 39.1059 TRY
+        "CAD" to 24.4831,   // 1 CAD = 24.4831 TRY
+        "AUD" to 21.7730,   // 1 AUD = 21.7730 TRY
+        "SEK" to 3.1795,    // 1 SEK = 3.1795 TRY
+        "DKK" to 4.8909,    // 1 DKK = 4.8909 TRY
+        "NOK" to 3.0820,    // 1 NOK = 3.0820 TRY
+        "SAR" to 9.3153,    // 1 SAR = 9.3153 TRY
+        "BRL" to 7.0,       // 1 BRL = 7.0 TRY
+        "MXN" to 20.3200,   // 1 MXN = 20.3200 TRY
+        "CZK" to 24.1600,   // 1 CZK = 24.1600 TRY
+        "PLN" to 4.0961,    // 1 PLN = 4.0961 TRY
+        "HUF" to 394.8500,  // 1 HUF = 394.8500 TRY
+        "RUB" to 104.1800,  // 1 RUB = 104.1800 TRY
+        "ZAR" to 18.6900,   // 1 ZAR = 18.6900 TRY
+        "KWD" to 113.3583,  // 1 KWD = 113.3583 TRY
+        "CNY" to 5.0,       // 1 CNY = 5.0 TRY
+        "CNH" to 5.1,       // 1 CNH = 5.1 TRY
+        "INR" to 0.4230,    // 1 INR = 0.4230 TRY
+        "AED" to 9.6000,    // 1 AED = 9.6000 TRY
+        "KRW" to 27.1750,   // 1 KRW = 27.1750 TRY
+        "HKD" to 4.5,       // 1 HKD = 4.5 TRY
+        "SGD" to 26.5000,   // 1 SGD = 26.5000 TRY
+        "MYR" to 7.4,       // 1 MYR = 7.4 TRY
+        "IDR" to 0.0023,    // 1 IDR = 0.0023 TRY
+        "THB" to 1.0,       // 1 THB = 1.0 TRY (Thai Baht)
+        "VND" to 0.0014,    // 1 VND = 0.0014 TRY
+        "PHP" to 0.62,      // 1 PHP = 0.62 TRY
+        "NZD" to 21.60,     // 1 NZD = 21.60 TRY
+        "BHD" to 93.0,      // 1 BHD = 93.0 TRY
+        "TWD" to 1.14,      // 1 TWD = 1.14 TRY
+        "MXV" to 35.30      // 1 MXV = 35.30 TRY (Mexican Unidad de Inversion)
+    )
+}
