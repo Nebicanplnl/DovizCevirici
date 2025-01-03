c4bf4adecebee68853c6330fb5bc59d2d60ffb72
Nebican Planalı <nebicanplanali@gmail.com>
2025-01-03 23:48:57 +0300

    Constants cleared

--- app/src/main/java/com/example/dovizcevirici/common/Constants.kt
+++ app/src/main/java/com/example/dovizcevirici/common/Constants.kt
@@ -5,8 +5,7 @@ object Constants {
     const val API_KEY = "4X1xshImsVF11DP4BUUoL7:6j8QyHwCajKGq6GZj6n38Q"
 
     const val BASE_URL = "https://api.collectapi.com/"
-    //https://api.collectapi.com/economy/exchange?base=USD&to=EUR&int=100
-    //https://api.collectapi.com/economy/exchange?int=10&to=EUR&base=USD
+
 
     const val CONTENT_TYPE = "application/json"
 
